feba5458e10e7617f7196659b4f9b0f13797be18
filip.cesnek <filip.cesnek@gmail.com>
2019-06-30 21:44:27 +0200

    update

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -1,9 +1,14 @@
+// Funkcija za iscrtavanje stupčastog grafa
+// Parametri: skup podataka, karakteristike koje će se prikazivati, HTML element u kojem
+// će se graf nalaziti, pokemonNum koji označava lijevog (1) ili desnog (2) Pokemona
 function drawBarChart(data, allowedKeys, element, pokemonNum) {
 
+    // Brisanje elementa pri novom crtanju
     if (d3.select(`#barchart${pokemonNum}`)) {
         d3.select(`#pokemon${pokemonNum}data`).selectAll("*").remove()
     }
 
+    // Margine, visina i širina grafa
     var marginBar = {
         top: 25,
         right: 25,
@@ -14,6 +19,7 @@ function drawBarChart(data, allowedKeys, element, pokemonNum) {
     var widthBar = 300 - marginBar.left - marginBar.right,
         heightBar = 250 - marginBar.top - marginBar.bottom;
 
+    // SVG element na kojem će se crtati graf
     var svgBar = d3.select(element).append("svg")
         .attr("width", widthBar + marginBar.left + marginBar.right)
         .attr("height", heightBar + marginBar.top + marginBar.bottom)
@@ -21,40 +27,48 @@ function drawBarChart(data, allowedKeys, element, pokemonNum) {
         .append("g")
         .attr("transform", "translate(" + marginBar.left + "," + marginBar.top + ")")
 
+    // Funkcija za X os na kojoj će se prikazivati vrijednost karakteristike
     var x = d3.scale.linear()
         .range([0, widthBar])
         .domain([0, d3.max(data, function (d) {
             return d.value;
         })]);
 
+    // Funkcija za Y os na kojoj će se ispisivati naziv karakteristike
     var y = d3.scale.ordinal()
         .rangeRoundBands([heightBar, 0], .1)
         .domain(data.map(function (d) {
             return d.axis;
         }));
 
+    // Boje pojedine karakteristike
     var colorBarFill = d3.scale.ordinal()
         .domain(allowedKeys)
         .range(["#FF0000", "#F08030", "#DF9A57", "#6890F0", "#78C850", "#F85888"]);
 
+    // Boje obruba pojedine karakteristike
     var colorBarBorder = d3.scale.ordinal()
         .domain(allowedKeys)
         .range(["#A60000", "#9C531F", "#A1871F", "#445E9C", "#4E8234", "#A13959"]);
 
+    // Definiranje Y osi sa skalom, brojem oznaka i orijentacijom
     var yAxis = d3.svg.axis()
         .scale(y)
         .tickSize(0)
         .orient("left");
 
+    // Dodavanje Y osi na SVG
     svgBar.append("g")
         .attr("class", "y axis")
         .call(yAxis)
 
+    // Dodavanje g elemenata za podatke
     var bars = svgBar.selectAll(".bar")
         .data(data)
         .enter()
         .append("g")
 
+    // Dodavanje rect elemenata na graf
     bars.append("rect")
         .attr("class", "bar")
         .attr("y", function (d) {
@@ -76,6 +90,7 @@ function drawBarChart(data, allowedKeys, element, pokemonNum) {
             return x(d.value);
         });
 
+    // Dodavanje teksta koji s nazivom i vrijednosti karakteristike
     bars.append("text")
         .style("opacity", 0)
         .transition()
--- scripts/radar.js
+++ scripts/radar.js
@@ -1,41 +1,67 @@
+// Funkcija za crtanje stupčastog grafa, ispis imena Pokemona, prikaz slike Pokemona i crtanje radarskog grafa
+// Parametri: skup podataka o Pokemonima, ID Pokemona koji se vizualizira, te pokemonNum (označava poziciju na
+// stranici, ako je lijevo, onda je 1, ako je desno onda je 2)
 function drawAll(data, id, pokemonNum) {
+    // margine, visina i širina radara
     var marginRadar = {
-            top: 100,
-            right: 100,
-            bottom: 100,
-            left: 100
-        },
+        top: 100,
+        right: 100,
+        bottom: 100,
+        left: 100
+    },
         widthRadar = Math.min(700, window.innerWidth - 10) - marginRadar.left - marginRadar.right,
         heightRadar = Math.min(widthRadar, window.innerHeight - marginRadar.top - marginRadar.bottom - 20);
 
+    // Prikaz imena i slike pokemona
     d3.select(`#pokemon${pokemonNum}name`).html(data[id - 1]["Name"])
     d3.select(`#pokemon${pokemonNum}Image`).attr("src", `https://www.serebii.net/art/th/${id}.png`)
+    // Inicijalizacija polja podataka za Pokemona koji se vizualizira
     pokeData[pokemonNum - 1] = []
 
+    // Ako se prvo odabire crtanje Pokemona na desnoj strani (drugi Pokemon), podaci
+    // za prvog Pokemona se postavljaju na 0 zbog načina na koji je biblioteka napisana
+    if (pokemonNum == 2 && pokeData[0].length == 0) {
+        for (key in allowedKeys) {
+            pokeData[0].push({
+                axis: allowedKeys[key],
+                value: 0
+            })
+        }
+    }
+    // Formatiranje podataka kako bi se mogla koristiti RadarChart biblioteka
     for (key in allowedKeys) {
         pokeData[pokemonNum - 1].push({
             axis: allowedKeys[key],
             value: data[id - 1][allowedKeys[key]]
         })
     }
+    
+    // Definiranje boja za radarski graf
     var colorRadar = d3.scale.ordinal()
-        .range(["#EDC951", "#6fad5e"]);
+        .range(["#EDC951", "#6FAD5E"]);
 
+    // Definiranje opcija za radarski graf
     var radarChartOptions = {
         w: widthRadar - 100,
         h: heightRadar - 100,
         margin: marginRadar,
-        maxValue: 100,
-        levels: 5,
+        maxValue: 120,
+        levels: 6,
         roundStrokes: false,
         color: colorRadar
     };
 
-    console.log(pokeData)
+    // Crtanje radarskog grafa i stupčastog grafa
+    // IZVORI:
+    // http://bl.ocks.org/nbremer/21746a9668ffdf6d8242
+    // https://gist.github.com/nbremer/21746a9668ffdf6d8242
+    // https://www.visualcinnamon.com/2015/10/different-look-d3-radar-chart.html
     RadarChart(".radarChart", pokeData, radarChartOptions);
     drawBarChart(pokeData[pokemonNum - 1], allowedKeys, `#pokemon${pokemonNum}data`, pokemonNum)
 }
 
+
+//Funkcija za pronalaženje ID-ja Pokemona u ovisnosti o predanom imenu
 function findPokemonByName(name, data) {
     return data.find(obj => obj.Name == name)["#"];
 }
